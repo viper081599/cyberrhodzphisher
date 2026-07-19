53122159bdb7211eb55b787b104698fe8afae0e9
viper081599 <reyesrodel506@gmail.com>
2026-07-19 18:21:13 +0800

    Update release URLs for cyberrhodzphisher

--- cyberrhodzphisher.sh
+++ cyberrhodzphisher.sh
@@ -156,9 +156,9 @@ kill_pid() {
 # Check for a newer release
 check_update(){
 	echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Checking for update : "
-	relase_url='https://api.github.com/repos/viper081599/cyberrhodzphisher/releases/latest'
+	relase_url='https://api.github.com/repos/htr-tech/zphisher/releases/latest'
 	new_version=$(curl -s "${relase_url}" | grep '"tag_name":' | awk -F\" '{print $4}')
-	tarball_url="https://github.com/viper081599/cyberrhodzphisher/archive/refs/tags/${new_version}.tar.gz"
+	tarball_url="https://github.com/htr-tech/zphisher/archive/refs/tags/${new_version}.tar.gz"
 
 	if [[ $new_version != $__version__ ]]; then
 		echo -ne "${ORANGE}update found\n"${WHITE}
@@ -168,13 +168,13 @@ check_update(){
 		curl --silent --insecure --fail --retry-connrefused \
 		--retry 3 --retry-delay 2 --location --output ".zphisher.tar.gz" "${tarball_url}"
 
-		if [[ -e ".cyberrhodzphisher.tar.gz" ]]; then
-			tar -xf .cyberrhodzzphisher.tar.gz -C "$BASE_DIR" --strip-components 1 > /dev/null 2>&1
+		if [[ -e ".zphisher.tar.gz" ]]; then
+			tar -xf .zphisher.tar.gz -C "$BASE_DIR" --strip-components 1 > /dev/null 2>&1
 			[ $? -ne 0 ] && { echo -e "\n\n${RED}[${WHITE}!${RED}]${RED} Error occured while extracting."; reset_color; exit 1; }
-			rm -f .cyberrhodzphisher.tar.gz
+			rm -f .zphisher.tar.gz
 			popd > /dev/null 2>&1
 			{ sleep 3; clear; banner_small; }
-			echo -ne "\n${GREEN}[${WHITE}+${GREEN}] Successfully updated! Run cyberrhodzphisher again\n\n"${WHITE}
+			echo -ne "\n${GREEN}[${WHITE}+${GREEN}] Successfully updated! Run zphisher again\n\n"${WHITE}
 			{ reset_color ; exit 1; }
 		else
 			echo -e "\n${RED}[${WHITE}!${RED}]${RED} Error occured while downloading."
@@ -184,6 +184,7 @@ check_update(){
 		echo -ne "${GREEN}up to date\n${WHITE}" ; sleep .5
 	fi
 }
+
 
 ## Check Internet Status
 check_status() {
